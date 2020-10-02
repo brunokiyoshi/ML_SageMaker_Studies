78c65d43554b02806dc29b2f62d7d21eae94460f
brunokiyoshi <ynumaru@gmail.com>
2020-10-02 00:06:25 +0000

    Define pytorch model and train it.

--- Project_Plagiarism_Detection/source_pytorch/model.py
+++ Project_Plagiarism_Detection/source_pytorch/model.py
@@ -27,7 +27,12 @@ class BinaryClassifier(nn.Module):
         super(BinaryClassifier, self).__init__()
 
         # define any initial layers, here
+        self.fc1 = nn.Linear(input_features, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.drop = nn.Dropout(0.2)
         
+        # sigmoid layer
+        self.sigmoid = nn.Sigmoid()
 
     
     ## TODO: Define the feedforward behavior of the network
@@ -37,8 +42,13 @@ class BinaryClassifier(nn.Module):
         :param x: A batch of input features of size (batch_size, input_features)
         :return: A single, sigmoid-activated value as output
         """
-        
+
         # define the feedforward behavior
+        x = F.relu(self.fc1(x))
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
         
-        return x
+        return x    
+        
     
